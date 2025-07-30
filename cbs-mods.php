738e7dcc83005f84bccea873b2ff6686b75b4091
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-07-30 12:18:48 -0400

    updated clamp values

--- cbs-mods.php
+++ cbs-mods.php
@@ -50,11 +50,11 @@ add_action('admin_notices', 'vm_bubble_blocks_admin_notice');
 function change_kadence_blocks_variable_font_sizes() {
   $arr = array(
     'sm'   => 'clamp(0.8rem, 0.73rem + 0.217vw, 0.9rem)',
-    'md'   => 'clamp(1.1rem, 0.995rem + 0.326vw, 1.313rem)',
-    'lg'   => 'clamp(1.75rem, 1.576rem + 0.543vw, 1.75rem)',
-    'xl'   => 'clamp(2rem, 1.728rem + 1.63vw, 2rem)',
-    'xxl'  => 'clamp(2.5rem, 1.456rem + 3.26vw, 3.25rem)',
-    'xxxl' => 'clamp(2.75rem, 0.489rem + 7.065vw, 4rem)',
+    'md'   => 'clamp(1.1rem, 0.995rem + 0.326vw, 1.313rem)', // regular
+    'lg'   => 'clamp(1.75rem, 1.576rem + 0.543vw, 1.75rem)', // large text
+    'xl'   => 'clamp(2rem, 1.728rem + 1.63vw, 2rem)', // x large text
+    'xxl'  => 'clamp(2.5rem, 1.456rem + 3.26vw, 3.25rem)', // heading 2
+    'xxxl' => 'clamp(2.75rem, 0.489rem + 7.065vw, 4rem)', // heading 1
   );
   return $arr;
 }
